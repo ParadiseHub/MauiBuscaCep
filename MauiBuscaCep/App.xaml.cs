3f36c0e642d98959e32fe38c2b71eed99c0cee64
Usuario <Usuario@CPD03-M10>
2024-05-13 11:19:36 -0300

    Adicionar arquivos de projeto.

---
+++ MauiBuscaCep/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBuscaCep
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MauiBuscaCep/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiBuscaCep
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
